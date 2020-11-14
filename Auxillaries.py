9a6f9decdbc06ed3158f226888f4458f93b4b370
yumao-gu <yumao.gu@student.maastrichtuniversity.nl>
2020-11-14 19:10:37 +0100

    guassian

--- Auxillaries.py
+++ Auxillaries.py
@@ -1,38 +1,79 @@
 # Loading libraries
 import numpy as np
 import matplotlib.pyplot as plt
-import torch
 import scipy as sp
-from scipy.stats import norm
+from scipy.stats import norm,multivariate_normal
+test = True
 
-def gaussian_mixture_model(means, stds, weights, n_samples, dim=1):
+def gaussian_model(pos = None,
+                                            mean = [0.5, -0.2],
+                                            cov = [[2.0, 0.3], [0.3, 0.5]],
+                                            test = test):
+    '''
+    Create data of a N-dim gaussian model
+
+    Arguments:
+        - means, cov: should be 1xN and NxN matrix
+        - pos: the input of sample points
+        - test: test flag
+
+    Outputs:
+        - value: : the gaussian output of sample points
+    '''
+    model = multivariate_normal(mean,cov)
+    if test:
+        x, y = np.mgrid[-1:1:.01, -1:1:.01]
+        pos = np.dstack((x, y))
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.contourf(x, y, model.pdf(pos))
+        plt.show()
+    return model.pdf(pos)
+
+def gaussian_mixture_model(pos = None,
+                                                                means = [[0.9, -0.8],[-0.7, 0.9]],
+                                                                covs = [[[2.0, 0.3], [0.3, 0.5]],
+                                                                                [[1.0, 0.8], [0.5, 0.9]]],
+                                                                weights = [0.3,0.7],
+                                                                test = test):
     '''
     Create data of a gaussian mixture model
 
     Arguments:
-        - means, stds: lists containing means and standard deviations of the single gaussian that are mixed
+        - pos: the input of sample points
+        - means, covs: lists containing means and standard deviations of \
+                                        the single gaussian that are mixed. Note, the cov\
+                                        matrix must be semidefine
         - weights: weight of each used gaussian when combining
-        - dim: Not sure if necessary: can be used for multidimensional gaussians
-        - n_samples: the amount of data points to be sampled from the gaussian mixture model
+        - test: test flag
 
     Outputs:
-        - X: Data matrix containing the samples from the gaussian mixture model: (n_samples x dim)
-
+        - value: : the gaussian mixture output of sample points
     '''
 
     # make sure all lists are o same length and weights are well defined
-    assert len(means) == len(stds)
+    assert len(means) == len(covs)
     assert len(means) == len(weights)
     assert sum(weights) == 1
+    if not test:
+        assert len(pos) > 0
 
-    # create cache for data
-    X = np.zeros((dim,n_samples))
+    if test:
+        x, y = np.mgrid[-2:2:.01, -2:2:.01]
+        pos = np.dstack((x, y))
 
+    value = np.zeros(pos[:,:,0].shape)
     # sample from each gaussian and add up to obtain mixture model
     for i in range(len(means)):
-        X += weights[i] * np.random.normal(means[i], stds[i], (dim,n_samples))
+        value += weights[i] * gaussian_model(pos,means[i], covs[i],False)
 
-    return X
+    if test:
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.contourf(x, y, value,levels = 30)
+        plt.show()
+
+    return value
 
 def LL(theta, X):
 
@@ -64,7 +105,11 @@ def LL(theta, X):
     Scores_1 = np.exp(-1*Likelihoods)*(norm.pdf(X,theta[0, 1], 0.2)*(X-theta[0,1])*(theta[0,0]/0.2**2))
     #print(f'Size Scores_1: {Scores_1.shape}')
     Score[0,1] = np.mean(Scores_1, axis = 1)
-    
+
 
 
     return L_value, Score
+
+if __name__ == "__main__":
+    # gaussian_model(test = test)
+    gaussian_mixture_model(test = test)
