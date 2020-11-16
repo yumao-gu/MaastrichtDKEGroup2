4b585b6236ec92f907fa91e7fa8ec15cfb7e640b
carstengieshoff <58326589+carstengieshoff@users.noreply.github.com>
2020-11-16 13:04:11 +0100

    Update on Example 1

--- Testing.py
+++ Testing.py
@@ -12,34 +12,88 @@ Create data of a gaussian mixture model
 '''
 # Specify parameters for mixture gaussian model
 weights = [.5, .45, .05]
-means = [0., .75, 3]
-stds = [.2, .2, .2]
-n = 20000
+means = [[0.], [.75],[ 3.]]
+covs = [[.2**2], [.2**2], [.2**2]]
+n = 100
 
 # Create sampels of given model
-X = gaussian_mixture_model(means, stds, weights, n_samples = n)
+X = gaussian_mixture_model_sample(n, means, covs, weights, test=False)
 print(f'Shape of X:{X.shape}')
-'''
-Build Log-Likelihood function in dependence of parameters which are to be determined
-'''
-# initialize
-theta = np.array([[0.1,4]]) #to be randomly initialized
-print(f'Shape of theta: {theta.shape}')
 
-# Gradient ascent
-n_iterations = 100000
-lr = 0.05
+
+'''
+Creating Contourlpot of likelihood function in dependence of parameters 
+'''
+make_Contourplot = False
+ticks = 1000
+
+if make_Contourplot:
+    print('Creating Colourplot...')
+    x_lin = np.linspace(0, 5, ticks)  # range for mu
+    y_lin = np.linspace(0, 0.6, ticks) # range for rho
+
+    Colourplot = np.zeros((ticks, ticks))
+
+    for i in reversed(range(ticks)):
+        for j in range(ticks):
+            x = x_lin[j]
+            y = y_lin[ticks - i - 1]
+            params = np.array([[y,x]])
+            L, _ =LL(params, X)
+            Colourplot[i, j] = L
+
+    np.save('Contourmatrix', Colourplot)
+else:
+    print('Loading Colourplot...')
+    Colourplot = np.load('Contourmatrix.npy')
+
+
+'''
+Gradient Ascent
+'''
+n_iterations = 2000
+lr = 0.01
 now = datetime.datetime.now()
+n_runs = 20
 
-for t in range(n_iterations):
-    L, S = LL(theta, X)
-    theta = theta+lr*S
-    if t % 1000 == 0:
-        print(f'S: {S}')
-        print(f'Iteration: {t} \t| Log-Likelihood:{L} \t| Time needed: {datetime.datetime.now()-now} | rho: {theta[0,0]}, mu: {theta[0,1]} ')
-        now = datetime.datetime.now()
+# Initializing Loss as minusinifinty to make sure first run achieves higher likelihood
+max_likelihood = -1*np.inf
+# Initializing caches
+Scores_list = []
+#Hessians =
+
+# Starting Plot
+fig, ax = plt.subplots(1,1, figsize = (7,7))
+# Running Gradient Ascent multiple times
+
+for run in range(n_runs):
+
+    theta = np.array([[np.random.uniform(0,1), np.random.uniform(0,5)]])  # to be randomly initialized
+    trajectory = [theta]
+
+    for t in range(n_iterations):
+
+        L, S = LL(theta, X)
+        theta = theta+lr*S
+        trajectory.append(theta)
+
+        if t % 100 == 0:
+            print(f'Run: {run+1}\t| Iteration: {t} \t| Log-Likelihood:{L} \t|  rho: {theta[0,0]}, mu: {theta[0,1]}  |  Time needed: {datetime.datetime.now()-now}  ')
+            now = datetime.datetime.now()
+
+    ax.plot([theta[0,1] for theta in trajectory], [theta[0,0] for theta in trajectory], alpha = 0.8)
+
+    # Updating Quantities if new max is found
+    if L > max_likelihood:
+        print('New Maximum found')
+        max_likelihood = L
+        Scores_list.append(S)
+        #Hessians =
+
+ax.imshow(Colourplot, cmap='Reds', extent = [0,5,0,.6], aspect='auto') #extent = [0,5,0,6]
+ax.set_xlabel('mu')
+ax.set_ylabel('10*rho')
+plt.title('Contourplot of Log-Likelihood function with gradient ascent trajectories')
+plt.show()
 
 
-
-
-print(theta)
