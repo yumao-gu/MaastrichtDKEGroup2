bde6a500ce0b6147083232af81f0e9a2d4126139
carstengieshoff <58326589+carstengieshoff@users.noreply.github.com>
2020-11-16 18:24:58 +0100

    Update ConfidenceIntervals.py
    First Version of Bootstrap CIs

--- ConfidenceIntervals.py
+++ ConfidenceIntervals.py
@@ -2,6 +2,7 @@ import numpy as np # https://numpy.org/doc/
 #import statistics as stat # https://docs.python.org/3/library/statistics.html
 import scipy as sp
 import sympy as sym
+from sklearn.utils import resample
 
 
 def normal_CI(alpha, Scores, Hessians, theta_n_M):
@@ -61,4 +62,60 @@ def normal_CI(alpha, Scores, Hessians, theta_n_M):
 
 
 
-def boostrap_CI
+def boostrap_CI(X, alpha, theta_hat, num_bootstraps = 100000, lr, n_iterations = 100 ):
+
+    '''
+    Function to create CI via bootstrap method
+
+    Arguments:
+        - X: original dataset
+        - alpha: confidence parameter
+        - theta_hat: Estimate derived by the normal M-times run gradient ascent
+        - num_bootstraps: number of bootstrap samples to be created
+        - lr: learning rate used in gradient ascent
+        - n_iterations: number of steps used in gradient ascent
+
+    Intermediates:
+        - n: size of provided dataset
+        - Bootstrap_thetas: Cache to store the new, to be calculated, estimates of theta, dim: num_bootstraps x dim theta
+        - X_bootstrap: Bootstrap sample of X, size n
+        - CI_borders: lower and upper bounds of CI of each theta_i : dim 2x dim theta
+
+    Further Information:
+        - I expect the function tau to map the parameter vector to a single parameter,
+          thus we create a CI for every entry of the parameter vector.
+
+    '''
+
+    # number of data points
+    n = X.shape[1]
+
+    #Cache for saving Boostrap samples
+    Bootstrap_thetas = mp.zeros((num_bootstraps, theta_hat.shape[1])
+
+    # Looping over amount of repitions of method
+    for j in range(num_bootstraps):
+
+        #Creating bootrap sample from original data
+        X_bootstrap = resample(X, replace=True, n_samples= n))
+
+        # perform GA on new likelihhod function
+        theta = theta_hat # initialize theta as theta_hat
+
+        for t in range(n_iterations):
+            L, S, _ = LL(theta, X_bootstrap, calc_Hessian=False)
+            theta = theta + lr * S
+
+            #TODO This could do with some checking whether it converged or not. this also should need very little iterations -> really small lr?
+
+        # Saving sample of theta
+        Bootstrap_thetas[j,:] = theta
+
+    # Cache for borders of CIs first row lower bound, second row upper bound, each column corresponds to one entry of the parameter vector
+    CI_borders = np.zeros((2,theta_hat.shape[1]))
+
+    CI_borders[0,:] = np.quantile(Bootstrap_thetas, alpha/2, axis = 0)
+    CI_borders[1,:] = np.quantile(Bootstrap_thetas, 1-alpha/2, axis = 0)
+
+    return CI_borders
+
