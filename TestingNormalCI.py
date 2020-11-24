7bb1f15fa16c3dd9fa7c2c842092acd3120285bf
carstengieshoff <58326589+carstengieshoff@users.noreply.github.com>
2020-11-24 21:14:56 +0100

    Testing normal CI updated

--- TestingNormalCI.py
+++ TestingNormalCI.py
@@ -8,6 +8,7 @@ from ConfidenceIntervals import *
 '''
 Design of Model/Likelihood function to bet fitted
 '''
+# For Testing: standard normal distribution with learnable parameters expected value and standard deviation
 def LogLikelihood(theta, x):
 
     '''
@@ -40,97 +41,130 @@ Create data of a gaussian mixture model
 weights = [1]
 means = [[0.]]
 covs = [[1.]]
-n = 100
+theta_gt = np.array([[0,1]])
+
+type = 'normal'
 n_CIs = 100
+alpha = 0.05
+theta_gt = np.array([[0,1]])
+n_list = [10**i for i in range(1,7)]
+
+n_ga_iterations =2500
+n_ga_runs = 1
+lr = 0.005
 '''
 Creating Contourlpot of likelihood function in dependence of parameters 
 '''
 # TODO: This is not well developed to be used for any setting: it needs limits for the parameters, works only for 2D, ... etc.
-make_Contourplot = False
-make_plots = True
+#make_Contourplot = False
+#make_plots = False
 
-
-'''
-Creating n_CIs CIs. Storing borders in a dict.
-'''
-CI_border_dict = {}
-
-for CI_run in range(n_CIs):
-    print('-'*10,f'CI run: {CI_run}','-'*10)
-    # Create sampels of given model
-    X = gaussian_mixture_model_sample(n, means, covs, weights, test=False) # we can alternatively use pytorchs MixtureFamily
-    X = torch.from_numpy(X)
-
+CI_accuracy_dict = {}
+for n in n_list:
 
     '''
-    Gradient Ascent
+    Creating n_CIs CIs. Storing borders in a dict.
     '''
-    # Set parameters here
-    n_iterations = 2500 #max number of iterations # TODO: alternatively/ additionally a error-based threshold?
-    lr = 0.005 # learning rate / stepsize
-    n_runs = 1
+    CI_border_dict = {}
 
-    # Initializing Loss as minus infinity to make sure first run achieves higher likelihood
-    max_likelihood = -1*np.inf
-    # trajectory_dict is a cache to save the gradient ascent trajectory of all gradient ascent runs
-    trajectory_dict = {}
-
-    # Running Gradient Ascent multiple (M=n_runs) times
-    for run in range(n_runs):
-
-        # Create/ Initialize variable ' TODO: make initialization more flexible
-        theta = torch.tensor([[uniform.Uniform(0., .6).sample(),uniform.Uniform(0., 5.).sample()]], requires_grad = True)
-
-        # Run complete Gradient ascent
-        theta, L, trajectory = gradient_ascent_torch(func = LogLikelihood,
-                                               param=theta,
-                                               data=X,
-                                               max_iterations=n_iterations,
-                                               learningrate=lr,
-                                               run_id=run,
-                                               print_info=False)
-
-        # Save optimization trajectory
-        trajectory_dict.update({run : trajectory})
-        # Updating Quantities if new max is found
-
-        # compare likelihood value to previous runs
-        if L > max_likelihood:
-            # This takes forever if n is large. As it is torch implementation I don't see a way to get this faster
-            print(f'New Maximum found! old:{max_likelihood} -> new:{L}')
-
-            # Update highest likelihood and theta estimate
-            max_likelihood = L
-            theta_hat = theta.clone().data.numpy()
-
-            # get derivatives
-            Scores, Hessian = get_derivatives_torch(func=LogLikelihood,
-                                                    param=theta,
-                                                    data=X,
-                                                    print_dims=False)
-
-
-    CI = normal_CI(0.05, Scores, Hessian, theta_hat)
-
-    CI_border_dict.update({CI_run : CI})
-
-'''
-Testing CIs:
-'''
-print(f'All {n_CIs} normal CIs calculated! Testing these now!')
-theta_gt = np.array([[0,1]])
-# for mu = theta[0,0]
-sum_theta = np.zeros_like(theta_hat)
-
-for i in range(sum_theta.shape[1]):
     for CI_run in range(n_CIs):
-        lower = CI_border_dict[CI_run][0,i]
-        upper = CI_border_dict[CI_run][1,i]
+        print('-'*10,f'CI run: {CI_run}','-'*10)
+        # Create sampels of given model
+        X = gaussian_mixture_model_sample(n, means, covs, weights, test=False) # we can alternatively use pytorchs MixtureFamily
+        X = torch.from_numpy(X)
 
-        if lower <= theta_gt[0,i] <=upper:
-            sum_theta[0,i]+=1
 
-sum_theta = sum_theta/n_CIs
+        '''
+        Gradient Ascent
+        '''
+        # Set parameters here
+        n_iterations = n_ga_iterations #max number of iterations # TODO: alternatively/ additionally a error-based threshold?
+         # learning rate / stepsize
+        n_runs = n_ga_runs
 
-for i in range(sum_theta.shape[1]):
-    print(f'The accuracy for parameter theta_{i} was: {sum_theta[0,i]*100} % ')
+        # Initializing Loss as minus infinity to make sure first run achieves higher likelihood
+        max_likelihood = -1*np.inf
+        # trajectory_dict is a cache to save the gradient ascent trajectory of all gradient ascent runs
+        trajectory_dict = {}
+
+        # Running Gradient Ascent multiple (M=n_runs) times
+        for run in range(n_runs):
+
+            # Create/ Initialize variable ' TODO: make initialization more flexible
+            theta = torch.tensor([[uniform.Uniform(0., .6).sample(),uniform.Uniform(0., 5.).sample()]], requires_grad = True)
+
+            # Run complete Gradient ascent
+            theta, L, trajectory = gradient_ascent_torch(func = LogLikelihood,
+                                                   param=theta,
+                                                   data=X,
+                                                   max_iterations=n_iterations,
+                                                   learningrate=lr,
+                                                   run_id=run,
+                                                   print_info=False)
+
+            # Save optimization trajectory
+            trajectory_dict.update({run : trajectory})
+            # Updating Quantities if new max is found
+
+            # compare likelihood value to previous runs
+            if L > max_likelihood:
+                # This takes forever if n is large. As it is torch implementation I don't see a way to get this faster
+                print(f'New Maximum found! old:{max_likelihood} -> new:{L}')
+
+                # Update highest likelihood and theta estimate
+                max_likelihood = L
+                theta_hat = theta.clone().data.numpy()
+
+                # get derivatives
+                Scores, Hessian = get_derivatives_torch(func=LogLikelihood,
+                                                        param=theta,
+                                                        data=X,
+                                                        print_dims=False)
+
+
+        CI = normal_CI(alpha, Scores, Hessian, theta_hat)
+
+        CI_border_dict.update({CI_run : CI})
+
+    '''
+    Testing CIs:
+    '''
+    print(f'All {n_CIs} normal CIs calculated! Testing these now!')
+    # for mu = theta[0,0]
+    sum_theta = np.zeros_like(theta_hat)
+
+    for i in range(sum_theta.shape[1]):
+        for CI_run in range(n_CIs):
+            lower = CI_border_dict[CI_run][0,i]
+            upper = CI_border_dict[CI_run][1,i]
+
+            if lower <= theta_gt[0,i] <=upper:
+                sum_theta[0,i]+=1
+
+    sum_theta = sum_theta/n_CIs
+
+    CI_accuracy_dict.update({n: sum_theta})
+
+'''
+Saving file
+'''
+saving_path = str(type)+'CI_accuracy_nCI'+str(n_CIs)+'_a'+str(alpha)
+np.save(saving_path+'.npy', CI_accuracy_dict)
+
+'''
+Plot Accuracy in dependence of n
+'''
+CI_accuracies = np.array([CI_accuracy_dict[n] for n in  n_list])
+print(CI_accuracies.shape)
+
+for i in range(theta.shape[1]):
+    plt.plot(n_list, CI_accuracies[:,0,i], label=f'theta_{i}')
+
+plt.xlabel('number of samples')
+plt.ylabel('CI coverage')
+plt.hlines(1-alpha, xmin = np.min(n_list), xmax = np.max(n_list), linestyle ='dashed', linewidth = 0.4, color = 'grey', label = '1-alpha')
+plt.legend()
+plt.savefig(saving_path+'.png')
+plt.show()
+
+
