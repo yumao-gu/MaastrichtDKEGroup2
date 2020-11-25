65c85047afa7b6c57d4957c57676b48f9b303abb
Annakoufou <annakoufou@outlook.com>
2020-11-26 00:06:48 +0100

    EM from scratch

--- EMalgorithm.py
+++ EMalgorithm.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
 from sklearn.mixture import GaussianMixture
+from sklearn import datasets
+from scipy.stats import multivariate_normal
 # use seaborn plotting defaults
 # If this causes an error, you can comment it out.
 #import seaborn as sns
@@ -48,4 +50,113 @@ def plotfitGauss(x,density):
   plt.hist(x, bins = 80, density = True, alpha=0.5)
   plt.plot(xpdf, density, '-r')
   plt.xlim(-10, 20);
+
+def EMfromscratch(X,k,it):
+  mu,cov,z=initial(X,k)
+  x=np.transpose(X)
+  
+  for i in range(it):
+    w=Estep(x,mu,cov,z)
+    print('succsees estep')
+    print('weights',w)
+    print()
+    mu,cov,z=Mstep(x,w)
+    print('mu',mu)
+    print()
+    print('cov',cov)
+    print()
+    print('z',z)
+    print('succsses mstep')
+    
+    print()
+
+def initial(x,k):
+  #mean initialization
+  xdim=x.shape[0]
+  mu=np.random.choice(x[1], (k,xdim))
+  #print(mu)
+  
+  #semidefinetpositive initialized cov matrix
+  cov=[]
+  for i in range(k):
+    cov.append(datasets.make_spd_matrix(xdim))
+  cov=np.array(cov)
+  #initialixetion of latent variables
+  z=np.ones(k)
+  for i in range(k):
+    z[i]=1/k
+  return mu,cov,z
+  
+def Estep(x,mu,cov,z):
+  '''
+  input data must be shape (datapoints,dimentions)
+  '''
+  k=len(z)
+  pr=[]
+  for i in range(k):
+    #something might be wrong with the data shape ask*****
+    pri=multivariate_normal.pdf(x,mu[i],cov[i])
+    pr.append(pri)
+  pr=np.array(pr)
+  
+  #comp of latent variables=>wi=(pdf(mui,covi)*zi)/sum(pdf(mui,covi)*zi)
+  multofpr=[]
+  for i in range(k):
+    multofpr.append(z[i]*pr[i])
+  multofpr=np.array(multofpr)
+
+  denom=np.zeros(x.shape[0])
+  for j in range(x.shape[0]):
+    for i in range(k):
+      denom[j]=denom[j]+multofpr[i][j]
+
+  w=np.zeros((k,x.shape[0]))
+  for i in range(k):
+    for j in range(x.shape[0]):
+      w[i][j]=multofpr[i][j]/denom[j]
+
+  return w
+
+def Mstep(x,w):
+
+  k=w.shape[0]
+  d=w.shape[1]
+  xdim=x.shape[1]
+  postw=np.zeros((1,k))
+  for i in range(k):
+    for j in range(x.shape[0]):
+      postw[0][i]=postw[0][i]+w[i][j]
+  zq=postw/x.shape[0]
+  z=zq[0]
+
+  mu=np.zeros((k,xdim))
+  for i in range(k):
+    wtr=w[i].reshape(len(x),1)
+    numer=np.sum(wtr*x,axis=0)
+    den=z[i]*d
+    mu[i]=numer/den
+
+  cov=np.zeros((k,xdim,xdim))
+  for i in range(k):
+    wtr=w[i].reshape(len(x),1)
+    diff=x-mu[i]
+    #print(diff)
+    elm1=wtr*diff
+    #print(elm1.shape)
+    elm2=np.transpose(diff)
+    numer=np.dot(elm2,elm1)
+    #print(numer)
+    den=z[i]*d
+    cov[i]=numer/den
+
+  return mu,cov,z
+  
+ 
+
+
+
+
+
+
+
 
--- TestingEM.py
+++ TestingEM.py
@@ -1,16 +1,27 @@
 from Auxillaries import *
 from EMalgorithm import *
+import numpy as np
 
 weights = [.5, .5]
-means = [[0.],[ 3.]]
-covs = [[.2**2], [.2**2]]
-n = 10
+means = [[0.,0],[ 3.,5]]
+covs = [[.2**2,], [.2**2]]
+n = 4
 
 # Create sampels of given model
-X = gaussian_mixture_model_sample(n, means, covs, weights, test=False)
-#print(X)
+X = gaussian_mixture_model_sample(50)
+'''
+to do:
+check grafically
+check n=4 for singular matrix
+'''
 print(f'Shape of X:{X.shape}')
-print(X[0])
-x=X[0]
-d,m,c=EMsklearn(x,2)
-plotfitGauss(x,d)
+'''
+x=np.transpose(X)
+#print(x)
+mu,cov,z=initial(X,3)
+#print(cov)
+print(cov.shape)
+w=Estep(x,mu,cov,z)
+Mstep(x,w)
+'''
+EMfromscratch(X,2,10)
